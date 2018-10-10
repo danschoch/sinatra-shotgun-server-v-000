acd216ba70edbd03c02b5dc9ab66547936840e75
Daniel Schoch <d.schoch90@gmail.com>
2018-10-10 18:19:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! I BULIT THIS!"
+  get '/' do
+    "Welcome to your app!!!! I BUIlT THIS!"
   end
 
 end
